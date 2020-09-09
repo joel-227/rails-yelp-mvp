0e8c65c3eeb7edafbdb4b8a7df87c0f3cb351387
Joel Koh <joelkoh2012@gmail.com>
2020-09-10 00:38:39 +0800

    unfinished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+sakae_sushi = { name: "Sakae Sushi", address: "Parkway Parade", category: "japanese" }
+din_tai_fung = { name: "Din Tai Fung", address: "Suntec City", category: "chinese" }
+pasta_fresca = { name: "Pasta Fresca", address: "Siglap", category: "italian", phone_number: "6241 5560" }
+crystal_jade = { name: "Crystal Jade", address: "290 Orchard Road", category: "chinese" }
+witbier = { name: "WitBier", address: "14 Aliwal St", category: "belgian", phone_number: "6291 1984" }
+
+[sakae_sushi, din_tai_fung, pasta_fresca, crystal_jade, witbier].each do |attribute|
+  Restaurant.create!(attribute)
+end
